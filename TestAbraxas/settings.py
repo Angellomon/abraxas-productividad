72398835374eff523925b08e8fe95e5fb951bed1
Angellomon <angelmtzdiaz@gmail.com>
2018-09-20 01:29:14 -0500

    sessiones
    C:

--- TestAbraxas/settings.py
+++ TestAbraxas/settings.py
@@ -118,6 +118,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+SESSION_ENGINE= 'django.contrib.sessions.backends.cached_db'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
